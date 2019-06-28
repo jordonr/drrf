efb20e2b3b0eb4f056e945cc221ea1ff59b2d815
Jordon Replogle <jordonreplogle@gmail.com>
2019-06-28 00:36:25 -0700

    Help with looping through dirs

--- Drrf.hx
+++ Drrf.hx
@@ -34,3 +34,26 @@ class Drrf extends mcli.CommandLine
 	}
 
 }
+
+/*
+Recursive loop through all directories / files
+function recursiveLoop(directory:String = "path/to/") {
+  if (sys.FileSystem.exists(directory)) {
+    trace("directory found: " + directory);
+    for (file in sys.FileSystem.readDirectory(directory)) {
+      var path = haxe.io.Path.join([directory, file]);
+      if (!sys.FileSystem.isDirectory(path)) {
+        trace("file found: " + path);
+        // do something with file
+      } else {
+        var directory = haxe.io.Path.addTrailingSlash(path);
+        trace("directory found: " + directory);
+        recursiveLoop(directory);
+      }
+    }
+  } else {
+    trace('"$directory" does not exists');
+  }
+}
+
+*/
