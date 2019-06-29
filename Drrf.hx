7f1823987a55f08f8215c6df3fddb1c3a9eec01b
Jordon Replogle <jordonreplogle@gmail.com>
2019-06-29 00:14:18 -0700

    Finished

--- Drrf.hx
+++ Drrf.hx
@@ -1,13 +1,27 @@
+using StringTools;
+
 /**
-	Say hello.
-	Example inspired by ruby's "executable" lib example
+	Drrf - Delete Recursively Redundant Files
+	Recersively run through a directory and deletes files with given extension 
+	with the same name as the files with the extension you want to keep.
+
+	Example: Drrf /path/to/dir rar mp4 
 **/
+
 class Drrf extends mcli.CommandLine
 {
 	/**
-		Say it in uppercase?
+		Starting path to search
 	**/
-	public var loud:Bool;
+	public var path:String;
+	/**
+		Extension you want to keep
+	**/
+	public var keep:String;
+	/**
+		Extension you want to delete
+	**/
+	public var del:String;
 
 	/**
 		Show this message.
@@ -18,14 +32,9 @@ class Drrf extends mcli.CommandLine
 		Sys.exit(0);
 	}
 
-	public function runDefault(?name:String)
+	public function runDefault(path:String, keep:String, del:String)
 	{
-		if(name == null)
-			name = "World";
-		var msg = 'Hello, $name!';
-		if (loud)
-			msg = msg.toUpperCase();
-		Sys.println(msg);
+		recursiveLoop(path, keep, del);
 	}
 
 	public static function main()
@@ -33,27 +42,33 @@ class Drrf extends mcli.CommandLine
 		new mcli.Dispatch(Sys.args()).dispatch(new Drrf());
 	}
 
-}
+	/*
+	Recursive loop through all directories / files
+	*/
+	private function recursiveLoop(directory:String, keepExt:String, delExt:String) {
+		if (sys.FileSystem.exists(directory)) {
+			for (file in sys.FileSystem.readDirectory(directory)) {
+				var path = haxe.io.Path.join([directory, file]);
+				if (!sys.FileSystem.isDirectory(path)) {
+					var fExt:String = getFileExtension(path);
+					if(fExt == keepExt) {
+						var delPath = path.replace("." + keepExt, "." + delExt);
+						if (sys.FileSystem.exists(delPath)) {
+							sys.FileSystem.deleteFile(delPath);
+						}
+					}
+				} else {
+					var directory = haxe.io.Path.addTrailingSlash(path);
+					recursiveLoop(directory, keepExt, delExt);
+				}
+			}
+		} else {
+			trace('"$directory" does not exists');
+		}
+	}
 
-/*
-Recursive loop through all directories / files
-function recursiveLoop(directory:String = "path/to/") {
-  if (sys.FileSystem.exists(directory)) {
-    trace("directory found: " + directory);
-    for (file in sys.FileSystem.readDirectory(directory)) {
-      var path = haxe.io.Path.join([directory, file]);
-      if (!sys.FileSystem.isDirectory(path)) {
-        trace("file found: " + path);
-        // do something with file
-      } else {
-        var directory = haxe.io.Path.addTrailingSlash(path);
-        trace("directory found: " + directory);
-        recursiveLoop(directory);
-      }
-    }
-  } else {
-    trace('"$directory" does not exists');
-  }
-}
+	private function getFileExtension(fName:String) {
+		return fName.substring(fName.lastIndexOf(".")+1);
+	}
 
-*/
+}
